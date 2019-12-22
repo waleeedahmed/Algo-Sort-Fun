ec2dcb692b0fdd52cec769565ae8fb051443cae7
Waleed Ahmed <mwaleed.ahmed@outlook.com>
2019-12-22 12:21:26 +1100

    Before adding CSS modules

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
+import Header from '../UI/Header/Header';
 
 class Layout extends Component {
     render() {
         return (
             <Auxiliary>
+                <Header/>
                 <p>drawer</p>
-                <p>header</p>
                 {this.props.children}
                 <p>footer</p>    
             </Auxiliary>
---
+++ src/components/UI/Header/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const header = props => (
+    <header>this is header!</header>
+)
+
+export default header;
