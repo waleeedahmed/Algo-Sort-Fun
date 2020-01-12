67403e08575f1288a8a3bf86b12acad8710868b5
Waleed Ahmed <mwaleed.ahmed@outlook.com>
2020-01-12 22:03:03 +1100

    Successfully utilitized React Move

--- src/components/Body/Array/ArrayBuilder/ArrayBuilder.js
+++ src/components/Body/Array/ArrayBuilder/ArrayBuilder.js
@@ -10,20 +10,42 @@ import trans from './BuilderAnimation.css';
 // this component will generate a series of number divs
 class ArrayBuilder extends Component {
     
+
+    indexHelper = (currentIndex, i) => {
+
+        if (i < this.props.value.generatedNumArray.length - 1 ) {
+          //  console.log(document.querySelector(`div[data-key = "${i}"]`).offsetWidth.toString())
+
+            if (currentIndex === i) return document.querySelector(`div[data-key = "${i + 1}"]`).clientWidth.toString() + 'px'
+
+            else if (currentIndex + 1 === i) return '-' + document.querySelector(`div[data-key = "${i - 1}"]`).clientWidth.toString() + 'px'
+
+            else return '0' 
+        }
+
+    }
+
     render() {
         
         const emptyArray = <div style = {{fontSize: '1.98rem', padding: '0 10px'}}>Array currently empty</div>
-        let displayNums = this.props.value.generatedNumArray.map((currentElement, index) => {
-            
-            return  <div data-key = {index} 
-                    className = {this.props.value.bubbleIdx === index || this.props.value.bubbleIdx + 1 === index ? `${classes.Array} ${classes.CurrentSwap}` : classes.Arraycell} 
-                    key = {index} > 
-                        {index === this.props.value.generatedNumArray.length - 1 ? currentElement: currentElement + ','} 
-                    </div>     
+        let displayNums = this.props.value.generatedNumArray.map((currentElement, index, arr) => {
+            let currentIdx = this.props.value.bubbleIdx;
+            return  <Animate key = {index}
+                    default ={{left: 0}}
+                    duration ={100}
+                    easing ='easeQuadIn'>
+                    {data => (
+                        <div style = {{transform: `translate(${this.indexHelper(currentIdx, index)}`}}
+                            className = {currentIdx === index || currentIdx + 1 === index ? `${classes.Array} ${classes.CurrentSwap}` : classes.Arraycell} 
+                            key = {index} data-key = {index}> 
+                                {index === this.props.value.generatedNumArray.length - 1 ? currentElement: currentElement + ','} 
+                        </div> 
+                    )}
+                </Animate>     
             }
         )
 
-        if (!this.props.value.vsPressed) {
+        //if (!this.props.value.vsPressed) {
 
             return (
                 // ***ORIGINAL***
@@ -32,17 +54,19 @@ class ArrayBuilder extends Component {
                 </CSSTransition> 
             )
             
-        } else {
-            console.log(document.querySelector(`div[data-key = "0"]`))
+        //} else {
+            //console.log(document.querySelector(`div[data-key = "0"]`))
         
 
-            return (<div><Animate> 
-                {data => {
-                    document.querySelector(`div[data-key = "0"]`).style.transform = 'translate(100px, 0)' 
-                }}
-            </Animate>
-            </div>)
-        }
+        //     return (<Animate> 
+        //         {data => {
+        //             //<div style = {{transform: 'translate(100px, 0)'}}>Test</div>
+        //             document.querySelector(`div[data-key = "0"]`).style.transform = 'translate(100px, 0)' 
+        //         }
+        //         }
+        //     </Animate>
+        //     )
+        // }
     }
 } 
 
